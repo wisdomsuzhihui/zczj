cc570a725d46baa26b4dc500b0322f17e54a65bd
suzhihui <wisdom.su@163.com>
2017-04-17 18:15:22 +0800

    add layout

--- app/controllers/Index/index.js
+++ app/controllers/Index/index.js
@@ -11,31 +11,76 @@ Category.hasMany(New, {
   foreignKey: 'CategoryID'
 })
 exports.index = function (req, res) {
-  Channel.findAll({
-    where: {
-      ChannelCode: '64'
-    },
-    include: [ChannelContent],
-
-  }).then(function (banners) {
-
+  var _category = req.query.category,
+    page = req.query.page || 0,
+    pageSize = 14,
+    curPage = page;
+  // 1.有分类请求
+  if (page) {
     New.findAndCountAll({
       'where': {
-        'CategoryID': '53'
+        'CategoryID': _category
       },
-      limit: 10,
+      offset: curPage - 1 * pageSize,
+      limit: pageSize,
       order: [
         ['NewsID', 'DESC']
       ]
     }).then(function (news) {
-      // console.log(JSON.stringify(news.rows))
       console.log(news)
-      res.render('index/index', {
-        title: '首页',
-        banners: banners,
-        news: news
+      res.json(news)
+    })
+  }
+  // 2. 无分类请求
+  else {
+    Channel.findAll({
+      where: {
+        ChannelCode: '64'
+      },
+      include: [ChannelContent],
+
+    }).then(function (banners) {
+
+      New.findAndCountAll({
+        'where': {
+          // 'CategoryID': '53'
+        },
+        offset: 2,
+        limit: 2,
+        order: [
+          ['NewsID', 'DESC']
+        ]
+      }).then(function (news) {
+        // console.log(JSON.stringify(news.rows))
+        console.log(news)
+        res.render('index/index', {
+          title: '首页',
+          banners: banners,
+          news: news
+        })
       })
     })
+
+  }
+}
+exports.newsList = function (req, res) {
+  var category = req.query.cat || 0,
+    page = req.query.page || 0,
+    pageSize = 14,
+    curPage = page;
+
+  New.findAndCountAll({
+    'where': {
+      'CategoryID': category
+    },
+    offset: curPage - 1 * pageSize,
+    limit: pageSize,
+    order: [
+      ['NewsID', 'DESC']
+    ]
+  }).then(function (news) {
+    console.log(news)
+    res.json(news)
   })
 
 }
--- public/scripts/js/index/index.js
+++ public/scripts/js/index/index.js
@@ -6,3 +6,36 @@ dgdAd.owlCarousel({
   autoplayTimeout: 5000,
   autoplayHoverPause: true
 });
+
+// Ajax请求函数
+function funAjax(URL, method, cb) {
+  $.ajax({
+    url: URL,
+    cache: true,
+    type: method,
+    crossDomain: true
+  }).done(cb);
+}
+
+
+;
+(function () {
+  var loadBtn = document.querySelector('.button-load');
+  var curpage = 0,
+    cat = '',
+    url = '/?page=' + curpage + '&category=' + cat;
+  loadBtn.addEventListener('click', function () {
+    // https://github.com/jaredreich/notie
+    // notie.alert({
+    //   type: 1,
+    //   text: '加载中'
+    // })
+    getData()
+
+  })
+
+  function getData(cat, page) {
+    curpage++;
+
+  }
+})();
