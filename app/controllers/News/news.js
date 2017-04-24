ab908cd48e5c32005e0cecc8410f6f62f4765d73
suzhihui <wisdom.su@163.com>
2017-04-24 18:31:25 +0800

    news

--- app/controllers/News/news.js
+++ app/controllers/News/news.js
@@ -11,58 +11,81 @@ Channel.hasOne(ChannelContent, {
 Category.hasMany(New, {
   foreignKey: 'CategoryID'
 })
+
 exports.index = function (req, res) {
-  var _category = req.query.category,
-    page = req.query.page || 0,
-    pageSize = 14,
-    curPage = page;
-  // 1.有分类请求
-  if (page) {
-    New.findAndCountAll({
-      'where': {
-        'CategoryID': _category
-      },
-      offset: curPage * pageSize,
-      limit: pageSize,
-      order: [
-        ['NewsID', 'DESC']
-      ]
-    }).then(function (news) {
-      console.log(news)
-      res.json(news)
-    })
-  }
-  // 2. 无分类请求
-  else {
-    Channel.findAll({
-      where: {
-        ChannelCode: '64'
-      },
-      include: [ChannelContent],
+  Channel.findAll({
+    where: {
+      ChannelName: '资讯幻灯片'
+    },
+    include: [ChannelContent]
+  }).then(function (bannerList) {
+    console.log()
+    // console.log(bannerList)
+    FriendLinks.findAll({}).then(function (links) {
 
-    }).then(function (banners) {
-
-      New.findAndCountAll({
-        'where': {
-          'CategoryID': '53'
-        },
-        offset: 0,
-        limit: 14,
-        order: [
-          ['NewsID', 'DESC']
-        ]
-      }).then(function (news) {
-        FriendLinks.findAll({}).then(function (links) {
-
-          res.render('news/index', {
-            title: 'us',
-            banners: banners,
-            news: news,
-            links: links
-          })
-        })
+      res.render('news/index', {
+        title: '资讯首页',
+        bannerList: JSON.parse((JSON.stringify(bannerList))),
+        links: links
       })
     })
-
-  }
+  })
 }
+
+
+
+// exports.index = function (req, res) {
+//   var _category = req.query.category,
+//     page = req.query.page || 0,
+//     pageSize = 14,
+//     curPage = page;
+//   // 1.有分类请求
+//   if (page) {
+//     New.findAndCountAll({
+//       'where': {
+//         'CategoryID': _category
+//       },
+//       offset: curPage * pageSize,
+//       limit: pageSize,
+//       order: [
+//         ['NewsID', 'DESC']
+//       ]
+//     }).then(function (news) {
+//       console.log(news)
+//       res.json(news)
+//     })
+//   }
+//   // 2. 无分类请求
+//   else {
+//     Channel.findAll({
+//       where: {
+//         ChannelCode: '64'
+//       },
+//       include: [ChannelContent],
+
+//     }).then(function (banners) {
+
+//       New.findAndCountAll({
+//         'where': {
+//           'CategoryID': '53'
+//         },
+//         offset: 0,
+//         limit: 14,
+//         order: [
+//           ['NewsID', 'DESC']
+//         ]
+//       }).then(function (news) {
+//         FriendLinks.findAll({}).then(function (links) {
+
+//           res.render('news/index', {
+//             title: 'us',
+//             banners: banners,
+//             news: news,
+//             links: links
+//           })
+//         })
+//       })
+//     })
+
+//   }
+// }
